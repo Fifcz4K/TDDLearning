501253e9e951b01d88fd11f3fa240b164fd7f862
Felipe <fifcz4k@gmail.com>
2022-07-15 13:44:30 +0200

    refactor tests in LowBatteryAlarm in gtest examples

--- LearningGoogleTest/LowBatteryAlarm/test/LowBatteryAlarmTests.cpp
+++ LearningGoogleTest/LowBatteryAlarm/test/LowBatteryAlarmTests.cpp
@@ -4,6 +4,7 @@
 #include "m_adc_mock.h"
 
 using ::testing::Return;
+using ::testing::InSequence;
 
 extern "C"
 {
@@ -46,17 +47,15 @@ TEST_F(LowBatteryAlarmTests, LedOnAfter5LowMeasures)
 
 TEST_F(LowBatteryAlarmTests, NoLedActionWhen4TooLowThenCorrectThen4TooLowAgain)
 {
+    InSequence s;
     EXPECT_CALL(adcMock, getAdcValue())
-    .Times(9)
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1));
+    .Times(4)
+    .WillRepeatedly(Return(LOW_BATTERY_ADC_VALUE - 1));
+    EXPECT_CALL(adcMock, getAdcValue())
+    .WillOnce(Return(LOW_BATTERY_ADC_VALUE));
+    EXPECT_CALL(adcMock, getAdcValue())
+    .Times(4)
+    .WillRepeatedly(Return(LOW_BATTERY_ADC_VALUE - 1));
     EXPECT_CALL(ledMock, LedOff()).Times(0);
     EXPECT_CALL(ledMock, LedOn()).Times(0);
 
@@ -68,22 +67,36 @@ TEST_F(LowBatteryAlarmTests, NoLedActionWhen4TooLowThenCorrectThen4TooLowAgain)
 
 TEST_F(LowBatteryAlarmTests, LedOnWhen4TooLowThenCorrectThen5TooLow)
 {
+    InSequence s;
     EXPECT_CALL(adcMock, getAdcValue())
-    .Times(10)
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1))
-    .WillOnce(Return(LOW_BATTERY_ADC_VALUE - 1));
+    .Times(4)
+    .WillRepeatedly(Return(LOW_BATTERY_ADC_VALUE - 1));
+    EXPECT_CALL(adcMock, getAdcValue())
+    .WillOnce(Return(LOW_BATTERY_ADC_VALUE));
+    EXPECT_CALL(adcMock, getAdcValue())
+    .Times(5)
+    .WillRepeatedly(Return(LOW_BATTERY_ADC_VALUE - 1));
     EXPECT_CALL(ledMock, LedOff()).Times(0);
     EXPECT_CALL(ledMock, LedOn());
 
     for(uint8_t i = 0; i < 10; i++)
+    {
+        vbatCheck();
+    }
+}
+
+TEST_F(LowBatteryAlarmTests, LedStillOnWhenCorrectMeasureAfterAlarm)
+{
+    InSequence s;
+    EXPECT_CALL(adcMock, getAdcValue())
+    .Times(5)
+    .WillRepeatedly(Return(LOW_BATTERY_ADC_VALUE - 1));
+    EXPECT_CALL(ledMock, LedOn());
+    EXPECT_CALL(adcMock, getAdcValue())
+    .WillOnce(Return(LOW_BATTERY_ADC_VALUE));
+    EXPECT_CALL(ledMock, LedOff()).Times(0);
+
+    for(uint8_t i = 0; i < 6; i++)
     {
         vbatCheck();
     }
